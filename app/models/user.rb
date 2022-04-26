63d2d724d39f670c393cc7b35db8b92565121198
eapenzacharias <eapenzac@gmail.com>
2022-04-26 19:54:22 +0200

    Created devise-token-auth user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
+        extend Devise::Models
+        # Include default devise modules.
+        devise :database_authenticatable, :registerable,
+                :recoverable, :rememberable, :trackable, :validatable,
+                :confirmable, :omniauthable
+        include DeviseTokenAuth::Concerns::User
   has_one_attached :profile_image
 end
--- db/schema.rb
+++ db/schema.rb
@@ -72,11 +72,28 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_26_160939) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.date "date_of_birth"
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.boolean "allow_password_change", default: false
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "name"
+    t.string "nickname"
+    t.string "image"
+    t.string "email"
+    t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
