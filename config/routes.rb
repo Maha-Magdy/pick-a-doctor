d2b8cb29ce4799c19d1c312d44c03c2576dcdea9
eapenzacharias <eapenzac@gmail.com>
2022-04-26 22:31:35 +0200

    Added namespaced routes for auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :api, default: {format: :json} do
+    mount_devise_token_auth_for 'User', at: 'auth'
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
