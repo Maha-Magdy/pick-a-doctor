c935df6d67c9100d678cbbd2e4009804a4575c66
eapenzacharias <eapenzac@gmail.com>
2022-04-26 22:32:43 +0200

    Linter fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
-        # skip_before_action :verify_authenticity_token
-        # protect_from_forgery unless: -> { request.format.json? }
-        before_action :update_allowed_parameters, if: :devise_controller?
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  # skip_before_action :verify_authenticity_token
+  # protect_from_forgery unless: -> { request.format.json? }
+  before_action :update_allowed_parameters, if: :devise_controller?
 
-        protected
-        def update_allowed_parameters
-          devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name password password_confirmation email date_of_birth])
-        end
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: %i[first_name last_name password password_confirmation email date_of_birth])
+  end
 end
