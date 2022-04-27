08f37c1cb7a65e77893f3510fd89fc80ad3283dc
eapenzacharias <eapenzac@gmail.com>
2022-04-27 21:24:12 +0200

    Added test for sign in

--- spec/api/auth/auth_spec.rb
+++ spec/api/auth/auth_spec.rb
@@ -3,10 +3,6 @@ require 'rails_helper'
 
 RSpec.describe 'api/auth', type: :request do
   path '/api/auth' do
-    before do
-      User.create(first_name: 'Test1', last_name: 'User', email: 'test@example.com', password: 'password123',
-                  date_of_birth: '10-01-2000', password_confirmation: 'password123')
-    end
     post('User sign-up') do
       consumes 'application/json'
       parameter name: :user, in: :body, schema: {
@@ -17,8 +13,36 @@ RSpec.describe 'api/auth', type: :request do
       }
       response '200', 'success' do
         let(:user) do
-          { email: 'user2@test.com', password: '1234567', password_confirmation: '1234567', first_name: 'User2',
+          { email: 'test@example.com', password: '1234567', password_confirmation: '1234567', first_name: 'User2',
             last_name: 'Test2', date_of_birth: '12-10-2000' }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/auth/sign_in' do
+    before do
+      User.create(first_name: 'Test1', last_name: 'User', email: 'test@example.com', password: 'password123',
+                  date_of_birth: '10-01-2000', password_confirmation: 'password123')
+    end
+    post('User sign-in') do
+      consumes 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: { id: { type: :integer }, first_name: { type: :string }, last_name: { type: :string },
+                      email: { type: :string }, date_of_birth: { type: :date } },
+        required: %w[id first_name last_name email date_of_birth]
+      }
+      response '200', 'success' do
+        let(:user) do
+          { email: 'test@example.com', password: 'password123' }
+        end
+        run_test!
+      end
+      response '401', 'fail' do
+        let(:user) do
+          { email: 'test@example.com', password: 'wrongpassword' }
         end
         run_test!
       end
