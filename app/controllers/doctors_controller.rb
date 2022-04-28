2149bf03599bd6f3cf648c3c0aa5a1c0896362d2
eapenzacharias <eapenzac@gmail.com>
2022-04-28 19:15:47 +0200

    Created doctors controller and set routes

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,16 @@
+class DoctorsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
---
+++ spec/requests/api/doctors_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "Api::Doctors", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/api/doctors/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/api/doctors/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/api/doctors/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /update" do
+    it "returns http success" do
+      get "/api/doctors/update"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /delete" do
+    it "returns http success" do
+      get "/api/doctors/delete"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
