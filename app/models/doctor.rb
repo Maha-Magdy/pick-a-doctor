a30b7aa86becf080bb14d3c67e5d7e7c3edd2201
eapenzacharias <eapenzac@gmail.com>
2022-05-07 23:50:10 +0200

    Added validation to doctor model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,4 +2,7 @@ class Doctor < ApplicationRecord
   belongs_to :specialization
   has_one_attached :profile_image
   has_many :appointments
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
